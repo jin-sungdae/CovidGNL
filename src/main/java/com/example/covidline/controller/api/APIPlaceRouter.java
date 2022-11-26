843bfb65b2f3361808e6839815b7e6f220587c39
sjin <wlstjdeo00@gmail.com>
2022-11-27 00:15:39 +0900

    FEATURE: 함수형 api 만들기

--- src/main/java/com/example/covidline/controller/api/APIPlaceRouter.java
+++ src/main/java/com/example/covidline/controller/api/APIPlaceRouter.java
@@ -14,27 +14,64 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 @Configuration
 public class APIPlaceRouter {
 
-    @Bean
-    public RouterFunction<ServerResponse> placeRouter() {
-        return route().nest(path("/api/places"), builder -> builder
-                .GET("", req -> ServerResponse.ok().body(List.of("place1","place2")))
-                .POST("", req -> ServerResponse.ok().body(true))
-                .GET("/{placeId}", req -> ServerResponse.ok().body("place " + req.pathVariable("placeId")))
-                .PUT("/{placeId}", req -> ServerResponse.ok().body(true))
-                .DELETE("/{placeId}", req -> ServerResponse.ok().body(true))
-                ).build();
-    }
 
 //    @Bean
-//    public RouterFunction<ServerResponse> placeRouter(APIPlaceHandler apiPlaceHandler) {
-//        return route().nest(path("/api/places"), builder -> builder
-//                .GET("", apiPlaceHandler::getPlaces)
-//                .POST("", apiPlaceHandler::createPlaces)
-//                .GET("/{placeId}", apiPlaceHandler::getPlace)
-//                .PUT("/{placeId}", apiPlaceHandler::modifyPlace)
-//                .DELETE("/{placeId}", apiPlaceHandler::removePlace)
-//        ).build();
+//    public RouterFunction<ServerResponse> placeRouter() {
+//        return route()
+//                .GET("/api/places", req -> ServerResponse.ok().body(List.of("place1", "place2")))
+//                /*
+//                @GetMapping("/places")
+//                public List<String> getPlaces() { return List.of("place1", "place2"); }
+//                 */
+//                .POST("/api/places", req -> ServerResponse.ok().body(true))
+//                /*
+//                @PostMappint("/places")
+//                public Boolean createPlace() { return true; }
+//                 */
+//                .GET("/api/places/{placeId}", req -> ServerResponse.ok().body("place " + req.pathVariable("placeId")))
+//                /*
+//                @GetMapping("/places/{placeId}")
+//                public String getPlace(@PathVariable Integer placeId) { return "place " + placeId; }
+//                 */
+//                .PUT("/api/places/{placeId}", req -> ServerResponse.ok().body(true))
+//                /*
+//                @PutMapping("/places/{placeId}")
+//                public Boolean modifyPlace(@PathVariable Integer placeId) { return true; }
+//                 */
+//                .DELETE("/api/places", req -> ServerResponse.ok().body(true))
+//                /*
+//                @DeleteMapping("/places/{placeId}")
+//                public Boolean removePlace(@PathVariable Integer placeId) { return true; }
+//                 */
+//                .build();
 //    }
+
+
+    /*
+     *  route().nest() -> nest()에 공통 path를 넣어 줘서 길이를 줄일 수 있다.
+     */
+
+//    @Bean
+//    public RouterFunction<ServerResponse> placeRouter() {
+//        return route().nest(path("/api/places"), builder -> builder
+//                .GET("", req -> ServerResponse.ok().body(List.of("place1","place2")))
+//                .POST("", req -> ServerResponse.ok().body(true))
+//                .GET("/{placeId}", req -> ServerResponse.ok().body("place " + req.pathVariable("placeId")))
+//                .PUT("/{placeId}", req -> ServerResponse.ok().body(true))
+//                .DELETE("/{placeId}", req -> ServerResponse.ok().body(true)))
+//                .build();
+//    }
+
+    @Bean
+    public RouterFunction<ServerResponse> placeRouter(APIPlaceHandler apiPlaceHandler) {
+        return route().nest(path("/api/places"), builder -> builder
+                .GET("", apiPlaceHandler::getPlaces)
+                .POST("", apiPlaceHandler::createPlaces)
+                .GET("/{placeId}", apiPlaceHandler::getPlace)
+                .PUT("/{placeId}", apiPlaceHandler::modifyPlace)
+                .DELETE("/{placeId}", apiPlaceHandler::removePlace)
+        ).build();
+    }
 
 
 }
