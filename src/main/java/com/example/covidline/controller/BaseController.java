cbb5a9d3cfe20b4a1b4ea7e96feb256c774a7063
sjin <wlstjdeo00@gmail.com>
2022-11-27 05:04:38 +0900

    FIX : BaseController /error 수정

--- src/main/java/com/example/covidline/controller/BaseController.java
+++ src/main/java/com/example/covidline/controller/BaseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController implements ErrorController{
+public class BaseController {
     @GetMapping("/")
     public String root() {
         return "index";
