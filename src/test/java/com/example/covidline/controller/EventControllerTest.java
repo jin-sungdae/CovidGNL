e909b92a5bcf451b0111b29c2ffa1bc793c4bc00
sjin <wlstjdeo00@gmail.com>
2022-12-08 08:40:00 +0900

    FIX : build.gradle에서 database 연결 해줘야하는거 mysql-> h2로 변경 mysql 아직 설정 안했기 때문

--- src/test/java/com/example/covidline/controller/EventControllerTest.java
+++ src/test/java/com/example/covidline/controller/EventControllerTest.java
@@ -1,11 +1,49 @@
 package com.example.covidline.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 public class EventControllerTest {
+
+    private final MockMvc mvc;
+
+    public EventControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 이벤트 리스트 페이지")
     @Test
-    void test(){
+    void givenNothing_whenRequestingEventsPage_thenReturnsEventsPage() throws Exception{
+        // Given
+
+        // When & Then
+        mvc.perform(get("/events"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("event/index"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("events"));
+    }
+
+    @DisplayName("[view][GET] 이벤트 세부 정보 페이지")
+    @Test
+    void givenNothing_whenRequestingEventDetailPage_thenReturnsEventDetailPage() throws Exception {
+        // Given
+        long eventId = 1L;
+
+        // When & Then
+        mvc.perform(get("/events/" + eventId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("event/detail"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("event"));
     }
 }
